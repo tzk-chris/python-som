20898b27be7f4fe5a46dea575df00f60bb343e08
tanzikun <875804957@qq.com>
2021-01-04 17:56:14 +0800

    最长回文子串

---
+++ longest_huiwenzichuang.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+@File  : 最长回文子串.py
+@Author: Ekko
+@Date  : 2020/8/30 2020/08/30
+"""
+
+
+def longestPalindrome(s):
+    n = len(s)
+    dp = [[False] * n for _ in range(n)]
+    ans = ""
+    # 枚举子串的长度 l+1
+    for l in range(n):
+        # 枚举子串的起始位置 i，这样可以通过 j=i+l 得到子串的结束位置
+        for i in range(n):
+            j = i + l
+            if j >= len(s):
+                break
+            if l == 0:
+                dp[i][j] = True
+            elif l == 1:
+                dp[i][j] = (s[i] == s[j])
+            else:
+                dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
+            if dp[i][j] and l + 1 > len(ans):
+                ans = s[i:j + 1]
+    return ans
